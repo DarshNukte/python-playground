65e1a64b4630f62a7160e47f3c9957563ee59f1e
Darsh Nukte <darshf036@gmail.com>
2025-10-16 11:18:04 +0530

    Day-2 Added

---
+++ Day-2/Data-Types.py
@@ -0,0 +1,17 @@
+print("Hello"[-1])
+
+# String
+print("123" + "345")
+
+# Integers = Whole Number
+print(123 + 345)
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
---
+++ Day-2/Mathematical-Operators.py
@@ -0,0 +1,13 @@
+# print("My age: " + str(12))
+# print(123 + 456)
+# print(7-3)
+# print(3*2)
+# print(5/3)
+# print(6//3)
+# print(2**2)
+#
+# print(3 * 3 + 3 / 3 - 3)
+bmi = 84 / 1.65 ** 2
+print(bmi)
+print(int(bmi))
+print(round(bmi))
